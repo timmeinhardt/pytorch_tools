1463245d2ea7a19ea15f81b35c738591d203cf9d
Tim Meinhardt <meinhardt.tim@gmail.com>
2018-09-08 12:40:29 +0200

    Fix packaging for config files

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='pytorch_tools',
       packages=['pytorch_tools'],
+      package_dir={'':'src'},
       version='0.0.1',
-      install_requires=[])
+      install_requires=[],
+      package_data={'': ['config/*.yaml']},)
